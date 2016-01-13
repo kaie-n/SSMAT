950d122ae0c715ccbfc44e0ccef817f1488c8d6f
Abdul Qayyum <kaien@alyssaa.net>
2016-01-13 12:05:08 +0800

    version 0.1

---
+++ fbd-exercise/Game.ts
@@ -0,0 +1,20 @@
+﻿module fbd {
+
+    export class Game extends Phaser.Game {
+
+        constructor() {
+
+            super(450, 140, Phaser.AUTO, 'cannyvas', null);
+
+            this.state.add('Boot', Boot, false);
+            this.state.add('Preloader', Preloader, false);
+            this.state.add('MainMenu', MainMenu, false);
+            this.state.add('Question1', Question1, false);
+
+            this.state.start('Boot');
+
+        }
+
+    }
+
+} 
